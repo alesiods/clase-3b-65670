2ab624de16a0c4290b17f4a56a312f376e9624e1
alesiods <alesioadrover@gmail.com>
2025-01-07 07:48:59 -0300

    Promesas, Map y Actividad proyecto e-commerce

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Promesas from './componentes/Promesas/Promesas'
+import Map from './componentes/Map/Map'
+import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
+
+const App = () => {
+
+
+  return (
+    <div>
+      {/* <Promesas/> */}
+     {/*  <Map/> */}
+     <ItemListContainer/>
+
+    </div>
+  )
+}
+
+export default App
